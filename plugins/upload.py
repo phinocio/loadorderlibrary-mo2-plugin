865651e7c00adf0b55f454aa67db5faeabd7bebb
Phinocio <github@phinocio.com>
2023-09-17 21:18:21 -0600

    add the rest of games

--- plugins/upload.py
+++ plugins/upload.py
@@ -17,7 +17,38 @@ class LolMo2Upload(mobase.IPluginTool):
     _name = "Load Order Library Upload"
     _frontendUrl = "https://loadorderlibrary.com"
     _appDataDir = "LoadOrderLibrary"
-    _gameIds = {"Skyrim Special Edition": 4, "Starfield": 30}
+    _gameIds = {
+        "Cyberpunk 2077": 11,
+        "Dark Messiah of Might & Magic": 13,
+        "Dark Souls": 14,
+        "Darkest Dungeon": 12,
+        "Dragon Age II": 15,
+        "Dragon Age: Origins": 16,
+        "Dungeon Siege II": 17,
+        "Enderal": 28,
+        "Enderal SE": 29,
+        "Fallout 3": 6,
+        "Fallout 4": 8,
+        "Fallout 4 VR": 9,
+        "Fallout New Vegas": 7,
+        "Kerbal Space Program": 18,
+        "Kingdom Come: Deliverance": 19,
+        "Mirror's Edge": 20,
+        "Mount & Blade II: Bannerlord": 21,
+        "No Man's Sky": 22,
+        "STALKER Anomaly": 23,
+        "Stardew Valley": 24,
+        "Starfield": 30,
+        "Tale of Two Wastelands": 10,
+        "Morrowind": 1,
+        "Oblivion": 2,
+        "Skyrim": 3,
+        "Skyrim Special Edition": 4,
+        "Skyrim VR": 5,
+        "The Binding of Isaac: Rebirth": 25,
+        "The Witcher 3: Wild Hunt": 26,
+        "Zeus and Poseidon": 27,
+    }
     _apiToken = None
     _slug = None
 
